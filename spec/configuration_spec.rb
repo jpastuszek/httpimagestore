e91f6fe2d00ad3fac1c8b25f815dbf93d991fb0c
Jakub Pastuszek <jpastuszek@gmail.com>
2013-05-17 15:23:59 +0200

    loading all parsers for configuration spec

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -1,17 +1,20 @@
 require_relative 'spec_helper'
 require 'httpimagestore/configuration'
 
+Configuration::Scope.logger = Logger.new('/dev/null')
+
+require 'pathname'
+Pathname.glob('lib/httpimagestore/configuration/*.rb').each do |conf|
+	require conf.relative_path_from(Pathname.new 'lib').to_s
+end
+
 describe Configuration do
 	it 'should parse configuration file' do
 		Configuration.from_file(support_dir + 'full.cfg')
 	end
 
 	it 'should read configuration from string' do
-		Configuration.read(<<-EOF)
-		path {
-			"uri" "blah"
-		}
-		EOF
+		Configuration.read (support_dir + 'full.cfg').read
 	end
 end
 
