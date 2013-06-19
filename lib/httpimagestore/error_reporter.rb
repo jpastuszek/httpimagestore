4a6bca4edf1cc6047701113bfaac68f38448a261
WhatClinic.com <wcc@lucy.sigquit.net>
2013-06-19 16:03:13 +0200

    using yielded error

--- lib/httpimagestore/error_reporter.rb
+++ lib/httpimagestore/error_reporter.rb
@@ -4,42 +4,42 @@ class ErrorReporter < Controler
 			Rack::UnhandledRequest::UnhandledRequestError,
 			Configuration::S3NoSuchKeyError,
 			Configuration::NoSuchFileError
-		)	do
-			write_error 404, env['app.error']
+		)	do |error|
+			write_error 404, error
 		end
 
-		on error HTTPThumbnailerClient::UnsupportedMediaTypeError do
-			write_error 415, env['app.error']
+		on error HTTPThumbnailerClient::UnsupportedMediaTypeError do |error|
+			write_error 415, error
 		end
 
 		on error(
 			HTTPThumbnailerClient::ImageTooLargeError,
 			MemoryLimit::MemoryLimitedExceededError
-		) do
-			write_error 413, env['app.error']
+		) do |error|
+			write_error 413, error
 		end
 
-		on error Configuration::Thumbnail::ThumbnailingError do
-			status = defined?(env['app.error'].remote_error.status) ? env['app.error'].remote_error.status : 500
-			write_error status, env['app.error']
+		on error Configuration::Thumbnail::ThumbnailingError do |error|
+			status = defined?(error.remote_error.status) ? error.remote_error.status : 500
+			write_error status, error
 		end
 
 		on error(
 			HTTPThumbnailerClient::InvalidThumbnailSpecificationError,
 		 	Configuration::ZeroBodyLengthError
-		) do
-			write_error 400, env['app.error']
+		) do |error|
+			write_error 400, error
 		end
 
-		log.error "unhandled error while processing request: #{env['REQUEST_METHOD']} #{env['SCRIPT_NAME']}[#{env["PATH_INFO"]}]", env['app.error']
-		log.debug {
-			out = StringIO.new
-			PP::pp(env, out, 200)
-			"Request: \n" + out.string
-		}
+		on error StandardError do |error|
+			log.error "unhandled error while processing request: #{env['REQUEST_METHOD']} #{env['SCRIPT_NAME']}[#{env["PATH_INFO"]}]", error
+			log.debug {
+				out = StringIO.new
+				PP::pp(env, out, 200)
+				"Request: \n" + out.string
+			}
 
-		on error StandardError do
-			write_error 500, env['app.error']
+			write_error 500, error
 		end
 	end
 end
