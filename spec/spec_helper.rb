38229037a6086d54a1cb2820c31ef89428383b77
Jakub Pastuszek <jpastuszek@gmail.com>
2013-08-09 12:16:31 +0200

    fixed test server startup for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,21 +28,28 @@ def headers(url)
 	HTTPClient.new.get(url).headers
 end
 
+@@running_cmd = {}
 def start_server(cmd, pid_file, log_file, test_url)
+	if @@running_cmd[pid_file]
+		return if @@running_cmd[pid_file] == cmd
+		stop_server(pid_file) 
+	end
+
 	fork do
 		Daemon.daemonize(pid_file, log_file)
 		log_file = Pathname.new(log_file)
 		log_file.truncate(0) if log_file.exist?
 		exec(cmd)
 	end
-	return unless Process.wait2.last.exitstatus == 0
+
+	@@running_cmd[pid_file] = cmd
 
 	ppid = Process.pid
 	at_exit do
 		stop_server(pid_file) if Process.pid == ppid
 	end
 
-	Timeout.timeout(6) do
+	Timeout.timeout(10) do
 		begin
 			get test_url
 		rescue Errno::ECONNREFUSED
@@ -56,6 +63,7 @@ def stop_server(pid_file)
 	pid_file = Pathname.new(pid_file)
 	return unless pid_file.exist?
 
+	STDERR.puts http_client.get_content("http://localhost:3000/stats") if pid_file.to_s.include? 'httpimagestore'
 	pid = pid_file.read.strip.to_i
 
 	Timeout.timeout(20) do
@@ -65,6 +73,7 @@ def stop_server(pid_file)
 				sleep 0.1
 			end
 		rescue Errno::ESRCH
+			@@running_cmd.delete pid_file.to_s
 			pid_file.unlink
 		end
 	end
