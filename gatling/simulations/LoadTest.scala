48399455234990cbe9a1aa267eb8a8c572566af3
Jakub Pastuszek <jpastuszek@gmail.com>
2015-06-09 12:53:45 +0100

    reorganized load test

--- gatling/simulations/LoadTest.scala
+++ gatling/simulations/LoadTest.scala
@@ -19,86 +19,97 @@ class LoadTest extends Simulation {
     val specs = csv("specs.csv").records
     val edits = csv("edits.csv").records
 
-    val rnd = new scala.util.Random
+    val rnd = new scala.util.Random(42)
 
-    val upload_and_thumbnail =
-      forever {
-        feed(image_files)
+    val upload =
+      feed(image_files)
+      .exec(
+        http("Upload image")
+        .post("/iss/v2/thumbnails/pictures/${file_name}")
+        .body(RawFileBody("${file_name}"))
+        .check(
+          status.is(200),
+          regex(""".+\.jpg$"""),
+          regex("""([^\r]+)""").saveAs("store_path")
+        )
+      )
+
+    val thumbnail =
+      group("Thumbnail") {
+        exec((session) => {
+          session.set("spec", specs(rnd.nextInt(specs length)))
+        })
+        .exec(flattenMapIntoAttributes("${spec}"))
+        .group("Image") {
+          exec(
+            http("${name}")
+            .get("/iss/v2/thumbnails/pictures${store_path}?operation=${operation}&width=${width}&height=${height}&options=${options}")
+            .check(
+              status.is(200),
+              headerRegex("Content-Type", "^image/")
+            )
+          )
+        }
+      }
+
+    val thumbnail_data_uri =
+      group("Thumbnail") {
+        exec((session) => {
+          session.set("spec", specs(rnd.nextInt(specs length)))
+        })
+        .exec(flattenMapIntoAttributes("${spec}"))
+        .group("Data URI") {
+          exec(
+            http("${name}")
+            .get("/iss/v2/thumbnails/pictures${store_path}?operation=${operation}&width=${width}&height=${height}&options=${options}&data-uri=true")
+            .check(
+              status.is(200),
+              header("Content-Type").is("text/uri-list"),
+              substring(";base64,")
+            )
+          )
+        }
+      }
+
+    val edit =
+      group("Edit") {
+        exec((session) => {
+          session.set("edit", edits(rnd.nextInt(edits length)))
+        })
+        .exec(flattenMapIntoAttributes("${edit}"))
         .exec(
-          http("Upload image")
-          .post("/iss/v2/thumbnails/pictures/${file_name}")
-          .body(RawFileBody("${file_name}"))
+          http("${name}")
+          .get("/iss/v2/thumbnails/pictures${store_path}?operation=fit&width=165&height=165&rotate=${rotate}&crop_x=${crop_x}&crop_y=${crop_y}&crop_w=${crop_w}&crop_h=${crop_h}&edits=${edits}")
           .check(
             status.is(200),
-            regex(""".+\.jpg$"""),
-            regex("""([^\r]+)""").saveAs("store_path")
+            headerRegex("Content-Type", "^image/")
           )
         )
-        .pause(50 millisecond, 200 millisecond)
-        .repeat(10) {
-          group("Thumbnail") {
-            exec((session) => {
-              session.set("spec", specs(rnd.nextInt(specs length)))
-            })
-            .exec(flattenMapIntoAttributes("${spec}"))
-            .group("${name}") {
-              exec(
-                http("Image")
-                .get("/iss/v2/thumbnails/pictures${store_path}?operation=${operation}&width=${width}&height=${height}&options=${options}")
-                .check(
-                  status.is(200),
-                  headerRegex("Content-Type", "^image/")
-                )
-              )
-              .pause(50 millisecond, 200 millisecond)
-            }
-            .exec((session) => {
-              session.set("spec", specs(rnd.nextInt(specs length)))
-            })
-            .exec(flattenMapIntoAttributes("${spec}"))
-            .group("${name}") {
-              exec(
-                http("Data URI")
-                .get("/iss/v2/thumbnails/pictures${store_path}?operation=${operation}&width=${width}&height=${height}&options=${options}&data-uri=true")
-                .check(
-                  status.is(200),
-                  header("Content-Type").is("text/uri-list"),
-                  substring(";base64,")
-                )
-              )
-              .pause(50 millisecond, 200 millisecond)
-            }
-          }
-          .group("Edit") {
-            exec((session) => {
-              session.set("edit", edits(rnd.nextInt(edits length)))
-            })
-            .exec(flattenMapIntoAttributes("${edit}"))
-            .exec(
-              http("${name}")
-              .get("/iss/v2/thumbnails/pictures${store_path}?operation=fit&width=165&height=165&rotate=${rotate}&crop_x=${crop_x}&crop_y=${crop_y}&crop_w=${crop_w}&crop_h=${crop_h}&edits=${edits}")
-              .check(
-                status.is(200),
-                headerRegex("Content-Type", "^image/")
-              )
-            )
-            .pause(50 millisecond, 200 millisecond)
-          }
-        }
-    }
+      }
   }
 
   val httpImageStore = http.baseURL("http://127.0.0.1:3050")
     .disableWarmUp
     .disableCaching
 
-  val upload_and_thumbnail = scenario("Upload and thumbnail images")
+  val upload_and_process = scenario("Upload and process images")
     .exec(ImageStore.health_check)
     .exitHereIfFailed
-    .exec(FlexiAPI.upload_and_thumbnail)
+    .forever {
+      exec(
+        FlexiAPI.upload.pause(50 millisecond, 200 millisecond),
+        repeat(10) {
+          exec(
+            FlexiAPI.thumbnail.pause(50 millisecond, 200 millisecond),
+            FlexiAPI.thumbnail_data_uri.pause(50 millisecond, 200 millisecond),
+            FlexiAPI.edit.pause(50 millisecond, 200 millisecond)
+          )
+        }
+      )
+    }
 
   setUp(
-    upload_and_thumbnail.inject(rampUsers(20) over (300 seconds)).protocols(httpImageStore)
+    upload_and_process.inject(rampUsers(20) over (300 seconds)).protocols(httpImageStore)
   ).maxDuration(300 seconds)
   .assertions(
     global.failedRequests.percent.is(0),
